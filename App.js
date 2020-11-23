b7e9ec8fe5be1e330656432e7778bb9a1fb5851f
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-22 23:16:36 -0500

    Navigate between diferents screens

--- App.js
+++ App.js
@@ -1,34 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Caddys!</Text>
+      <Button title="Go Detail" onPress={() => navigation.push("Detail")} />
       <StatusBar style="auto" />
     </View>
   );
 };
 
-const DetailScreen = () => {
+const DetailScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Sandrica</Text>
+      <Button title="Back" onPress={() => navigation.goBack()} />
       <StatusBar style="auto" />
     </View>
   );
-}
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
+};
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Detail: {
+      screen: DetailScreen,
+    },
   },
-  Detail: {
-    screen: DetailScreen,
-  },
-},{initialRouteName: 'Detail'});
+  { initialRouteName: "Home" }
+);
 
 export default createAppContainer(AppNavigator);
 
