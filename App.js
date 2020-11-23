db379b2eae0f0a27b6550cead0138f4336da6be7
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-22 23:08:34 -0500

    Created 2 stack menu and define start screnn

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Caddys!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const DetailScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sandrica</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Detail: {
+    screen: DetailScreen,
+  },
+},{initialRouteName: 'Detail'});
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
